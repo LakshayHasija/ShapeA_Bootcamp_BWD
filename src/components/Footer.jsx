47eab0da44d0bede1a496de0e4c5321b864b8a4c
Lakshay Hasija <lakhay.hasija2002@gmail.com>
2021-06-07 16:05:05 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Footer() {
+var currYear = new Date().getFullYear();
+return (
+  <footer>
+    <p>Copyright @ {currYear}</p>
+  </footer>
+  Footer.jsx
+  document.get
+  stack frames w
+  );
+}
+export default Footer;
