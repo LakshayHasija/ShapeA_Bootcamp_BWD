470e915fb5d14392ff375d78036b6681ad0525ec
Lakshay Hasija <lakhay.hasija2002@gmail.com>
2021-06-07 16:04:57 +0530

    Create Note.jsx

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Note() {
+return (
+  <div className="note"
+    <h1>BootCamp</h1>
+      <p>
+      Loved this bootcamp.
+      </p>
+  </div>
+  );
+}
+export default Note;
